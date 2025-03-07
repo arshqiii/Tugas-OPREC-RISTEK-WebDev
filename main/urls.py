31182476227a29537e3bb877b0e88c985282c56e
arshqiii <marshq23@gmail.com>
2025-03-07 11:25:58 +0700

    implemented add and delete tryout

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,8 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-tryout/', tryout_create, name='create_tryout'),
+    path('edit_tryout/<int:id>/', tryout_edit, name='tryout_edit'),
+    path('delete_tryout/<int:id>/', tryout_delete, name='tryout_delete'),
+    path('detail_tryout/<int:id>/', tryout_detail, name='tryout_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,3 @@
-# quiz/views.py
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
@@ -8,7 +7,7 @@ from .forms import TryoutForm, QuestionForm
 def show_main(req):
     tryout = Tryout.objects.all()
     context = {
-        'list_tryout' : tryout
+        'list_tryout': tryout
     }
     return render(req, "mainPage/index.html", context)
 
@@ -17,26 +16,38 @@ def tryout_create(req):
         form = TryoutForm(req.POST)
         if form.is_valid():
             form.save()
-            return redirect('tryout_list')
+            return redirect('main:show_main')
     else:
         form = TryoutForm()
     context = {'form': form}
-    return render(req, 'addTryout/index.html', context) 
+    return render(req, 'addTryout/index.html', context)
 
 def tryout_edit(req, id):
-    tryout = Tryout.objects.get(pk=id)
-    form = TryoutForm(req.POST, instance=tryout)
-    if req.method == 'POST' and form.is_valid() :
-        form.save()
-        return HttpResponseRedirect(reverse('main:tryout_detail'))
-    
-    context = {'form': form}
+    tryout = get_object_or_404(Tryout, pk=id)
+
+    if req.method == 'POST':
+        form = TryoutForm(req.POST, instance=tryout)
+        if form.is_valid():
+            form.save()
+            return redirect('main:tryout_detail', id)  # Perbaiki di sini!
+
+    else:
+        form = TryoutForm(instance=tryout)
+
+    context = {'form': form, 'tryout': tryout}
     return render(req, 'editTryout/index.html', context)
 
-def tryout_delete(req,id):
-    tryout = Tryout.objects.get(pk=id)
+def tryout_delete(req, id):
+    tryout = get_object_or_404(Tryout, pk=id)
     tryout.delete()
-    return HttpResponseRedirect(reverse('main:show_main'))
+    return redirect('main:show_main')
 
 def tryout_detail(req, id):
-    pass
+    tryout = get_object_or_404(Tryout, pk=id)
+    questions = Question.objects.filter(tryout=tryout)
+    
+    context = {
+        'tryout': tryout,
+        'questions': questions
+    }
+    return render(req, 'detailTryout/index.html', context)
