f376d21e2d26f9ca2da66e253ca914219705298e
arshqiii <marshq23@gmail.com>
2025-03-07 10:32:28 +0700

    implemented show_main + views & urls

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# quiz/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Tryout, Question
+from .forms import TryoutForm, QuestionForm
 
-# Create your views here.
+def show_main(req):
+    tryout = Tryout.objects.all()
+    context = {
+        'list_tryout' : tryout
+    }
+    return render(req, "mainPage/index.html", context)
