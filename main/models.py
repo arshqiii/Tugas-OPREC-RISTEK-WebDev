5a786969a15b83121fd9a686a2d306f6c0fb9d4f
arshqiii <marshq23@gmail.com>
2025-03-07 10:07:00 +0700

    added Tryout & Question Model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Tryout(models.Model) :
+    title = models.CharField(max_length=300)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Question(models.Model):
+    tryout = models.ForeignKey(Tryout, on_delete=models.CASCADE, related_name='questions')
+    text = models.TextField()
+    answer = models.BooleanField()
