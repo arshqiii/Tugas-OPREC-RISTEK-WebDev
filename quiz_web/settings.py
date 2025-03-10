d463b31a815115f87e5700153fab575017b2ffdc
arshqiii <marshq23@gmail.com>
2025-03-10 21:15:02 +0700

    modified database to sqlite

--- quiz_web/settings.py
+++ quiz_web/settings.py
@@ -88,15 +88,10 @@ WSGI_APPLICATION = 'quiz_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'quiz-web-oprec',
-        'USER': 'postgres',
-        'PASSWORD': 'p455word',
-        'HOST': 'localhost',
-        'PORT': '5432',  # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
